56963f250e160eba260fecd3df61c113c017118a
Lorenzo Bertoni <lorenzobertoni9@gmail.com>
2021-08-02 13:39:52 +0100

    add default to the parser

--- demo/mono_det_demo.py
+++ demo/mono_det_demo.py
@@ -8,12 +8,15 @@ from mmdet3d.apis import (inference_mono_3d_detector, init_model,
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('image', help='image file')
-    parser.add_argument('ann', help='ann file')
-    parser.add_argument('config', help='Config file')
-    parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument('images', nargs='*', help='input images')
+    parser.add_argument('--ann', help='ann file', default='demo/data/wayve/test.json')
+    parser.add_argument('--config', help='Config file',
+                        default='configs/fcos3d/fcos3d_r101_caffe_fpn_gn-head_dcn_2x8_1x_nus-mono3d_finetune.py')
+    parser.add_argument('--checkpoint', help='Checkpoint file',
+                        default='checkpoints/fcos3d_r101_caffe_fpn_gn-head_dcn_2x8_1x_nus-mono3d_finetune_20210717_095645-8d806dc2.pth')
     parser.add_argument(
         '--device', default='cuda:0', help='Device used for inference')
+    parser.add_argument('--glob', help='glob expression for input images (for many images)')
     parser.add_argument(
         '--score-thr', type=float, default=0.15, help='bbox score threshold')
     parser.add_argument(
@@ -26,11 +29,16 @@ def main():
         help='whether to save online visuliaztion results')
     args = parser.parse_args()
 
+    if args.glob:
+        args.images += glob.glob(args.glob)
+    if not args.images:
+        raise Exception("no image files given")
+
     # build the model from a config file and a checkpoint file
     model = init_model(args.config, args.checkpoint, device=args.device)
     # test a single image
 
-    for image in glob.glob(osp.join('/home/lorenzo/images/runs/short_short', '*.jpeg')):
+    for image in args.images:
         print(f'Running inference on image {image}')
         result, data = inference_mono_3d_detector(model, image, args.ann)
         # show the results
